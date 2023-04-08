58b97337964a42325176718eb3314b31b67f586b
Andy Phan <andyphan@live.com.au>
2023-04-08 10:04:54 +0930

    Implemented mobile menu

--- menu.js
+++ menu.js
@@ -1,7 +1,19 @@
 const subMenu = document.querySelectorAll('.expandable');
+const navToggler = document.querySelector('.nav-toggler');
+const navbar = document.querySelector('.navbar');
+
+navToggler.addEventListener('click', () => {
+    navbar.classList.toggle('collapse');
+    if(navbar.classList.contains('collapse')) {
+        navToggler.firstElementChild.src = "images/icon-hamburger.svg";
+    } else {
+        navToggler.firstElementChild.src = "images/icon-close.svg";
+    }
+    
+});
 
 subMenu.forEach(element => {
     element.addEventListener('click', () => {
         element.querySelector('.sub-menu').classList.toggle('expand');
     })
-})
+});
